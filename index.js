c28635e8688ed79b121dfe46b14a868a8443bb15
BryanDev1 <bolangebryan@gmail.com>
2024-10-15 11:13:53 +0200

    changes names

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import chalk from "chalk";
-const name1="Willy"
-const name2="Dwayne"
-const name3= "Remi"
-const name4= "John"
+const name1="Marina"
+const name2="Aude"
+const name3= "Claire"
 console.log(chalk.blue("Voici mes collègues : " + 
     chalk.red(name1) + " " + 
     chalk.green(name2) + " " + 
-    chalk.magenta(name3) + " " + 
-    chalk.grey(name4)
+    chalk.magenta(name3) 
 ));
